af0e1fbd6fb903b74737d99bb6f965af421b8f90
norah24hm <norah24hm@gmail.com>
2024-07-09 03:11:50 +0300

    control-panel

---
+++ robot-control-panel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="cont-style.css">
+
+</head>
+<body>
+    
+    <div class="container">
+        <form method="post" action="db-connection.php">
+            <button name="direction" value="forward">^</button><br>
+            <button name="direction" value="left"><</button>
+            <button name="direction" value="stop">Stop</button>
+            <button name="direction" value="right"> > </button><br>
+            <button name="direction" value="backward">v</button>
+        </form>
+    </div>
+</body>
+</html>
